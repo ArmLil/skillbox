e300b9fb5cf6b2d1d2e526506246830fb9f69ff7
ArmLil <armlilhov@mail.ru>
2022-02-10 23:59:15 +0300

    add loader to orderInfo

--- src/pages/OrderInfoPage.vue
+++ src/pages/OrderInfoPage.vue
@@ -1,118 +1,165 @@
 <template lang="html">
-  <main class="content container">
-    <div class="content__top">
-      <ul class="breadcrumbs">
-        <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" href="index.html">
-            Каталог
-          </a>
-        </li>
-        <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" href="cart.html">
-            Корзина
-          </a>
-        </li>
-        <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link">
-            Оформление заказа
-          </a>
-        </li>
-      </ul>
+  <div>
+    <main class="content container" v-if="orderInfoLoading"><Loader class="info" /></main>
+    <main class="content container" v-else-if="orderInfoLoadingFailed">
+      <div class="info">Информация про заказ не загрузилась</div>
+    </main>
+    <main class="content container" v-else>
+      <div class="content__top">
+        <ul class="breadcrumbs">
+          <li class="breadcrumbs__item">
+            <a class="breadcrumbs__link" href="index.html">
+              Каталог
+            </a>
+          </li>
+          <li class="breadcrumbs__item">
+            <a class="breadcrumbs__link" href="cart.html">
+              Корзина
+            </a>
+          </li>
+          <li class="breadcrumbs__item">
+            <a class="breadcrumbs__link">
+              Оформление заказа
+            </a>
+          </li>
+        </ul>
 
-      <h1 class="content__title">Заказ оформлен <span>№ 23621</span></h1>
-    </div>
+        <h1 class="content__title">
+          Заказ оформлен <span>№ {{ orderInfo.id }}</span>
+        </h1>
+      </div>
 
-    <section class="cart">
-      <form class="cart__form form" action="#" method="POST">
-        <div class="cart__field">
-          <p class="cart__message">
-            Благодарим за&nbsp;выбор нашего магазина. На&nbsp;Вашу почту придет письмо
-            с&nbsp;деталями заказа. Наши менеджеры свяжутся с&nbsp;Вами в&nbsp;течение часа для
-            уточнения деталей доставки.
-          </p>
-
-          <ul class="dictionary">
-            <li class="dictionary__item">
-              <span class="dictionary__key">
-                Получатель
-              </span>
-              <span class="dictionary__value">
-                {{ orderInfo.name }}
-              </span>
-            </li>
-            <li class="dictionary__item">
-              <span class="dictionary__key">
-                Адрес доставки
-              </span>
-              <span class="dictionary__value">
-                {{ orderInfo.address }}
-              </span>
-            </li>
-            <li class="dictionary__item">
-              <span class="dictionary__key">
-                Телефон
-              </span>
-              <span class="dictionary__value">
-                {{ orderInfo.phone }}
-              </span>
-            </li>
-            <li class="dictionary__item">
-              <span class="dictionary__key">
-                Email
-              </span>
-              <span class="dictionary__value">
-                {{ orderInfo.email }}
-              </span>
-            </li>
-            <li class="dictionary__item">
-              <span class="dictionary__key">
-                Способ оплаты
-              </span>
-              <span class="dictionary__value">
-                картой при получении
-              </span>
-            </li>
-          </ul>
-        </div>
-
-        <div class="cart__block">
-          <ul class="cart__orders">
-            <li class="cart__order" v-for="item in orderInfo.basket.items" :key="item.id">
-              <h3>{{ item.product.title }}</h3>
-              <b>{{ item.product.price | NumberFormat }} ₽</b>
-              <span>Артикул: {{ item.product.id }}</span>
-              <span>Количество: {{ item.quantity }}</span>
-            </li>
-          </ul>
-
-          <div class="cart__total">
-            <p>Доставка: <b>500 ₽</b></p>
-            <p>
-              Итого: <b>3</b> товара на сумму <b>{{ orderInfo.totalPrice | NumberFormat }} ₽</b>
+      <section class="cart">
+        <form class="cart__form form" action="#" method="POST">
+          <div class="cart__field">
+            <p class="cart__message">
+              Благодарим за&nbsp;выбор нашего магазина. На&nbsp;Вашу почту придет письмо
+              с&nbsp;деталями заказа. Наши менеджеры свяжутся с&nbsp;Вами в&nbsp;течение часа для
+              уточнения деталей доставки.
             </p>
+
+            <ul class="dictionary">
+              <li class="dictionary__item">
+                <span class="dictionary__key">
+                  Получатель
+                </span>
+                <span class="dictionary__value">
+                  {{ orderInfo.name }}
+                </span>
+              </li>
+              <li class="dictionary__item">
+                <span class="dictionary__key">
+                  Адрес доставки
+                </span>
+                <span class="dictionary__value">
+                  {{ orderInfo.address }}
+                </span>
+              </li>
+              <li class="dictionary__item">
+                <span class="dictionary__key">
+                  Телефон
+                </span>
+                <span class="dictionary__value">
+                  {{ orderInfo.phone }}
+                </span>
+              </li>
+              <li class="dictionary__item">
+                <span class="dictionary__key">
+                  Email
+                </span>
+                <span class="dictionary__value">
+                  {{ orderInfo.email }}
+                </span>
+              </li>
+              <li class="dictionary__item">
+                <span class="dictionary__key">
+                  Способ оплаты
+                </span>
+                <span class="dictionary__value">
+                  картой при получении
+                </span>
+              </li>
+            </ul>
           </div>
-        </div>
-      </form>
-    </section>
-  </main>
+
+          <div class="cart__block">
+            <ul class="cart__orders">
+              <li class="cart__order" v-for="item in orderInfo.basket.items" :key="item.id">
+                <h3>{{ item.product.title }}</h3>
+                <b>{{ item.product.price | NumberFormat }} ₽</b>
+                <span>Артикул: {{ item.product.id }}</span>
+                <span>Количество: {{ item.quantity }}</span>
+              </li>
+            </ul>
+
+            <div class="cart__total">
+              <p>Доставка: <b>500 ₽</b></p>
+              <p>
+                Итого: <b>3</b> товара на сумму <b>{{ orderInfo.totalPrice | NumberFormat }} ₽</b>
+              </p>
+            </div>
+          </div>
+        </form>
+      </section>
+    </main>
+  </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 import NumberFormat from "@/helpers/numberFormat";
+import Loader from "@/components/Loader.vue";
 
 export default {
+  data() {
+    return {
+      orderInfoLoadingFailed: false,
+      orderInfoLoading: false
+    };
+  },
+  components: { Loader },
   computed: mapState({
-    orderInfo: state => state.orderInfo
+    orderInfo: state => {
+      return state.orderInfo;
+    }
   }),
+  methods: {
+    loadCurrentOrderInfo() {
+      this.orderInfoLoading = true;
+      this.orderInfoLoadingFailed = false;
+      return new Promise(resolve => setTimeout(resolve, 2000)).then(() => {
+        return this.$store
+          .dispatch("loadOrderInfo", this.$route.params.id)
+          .catch(() => {
+            this.orderInfoLoadingFailed = true;
+          })
+          .then(() => {
+            this.orderInfoLoading = false;
+          });
+      });
+    }
+  },
+  watch: {
+    "$route.params.id": {
+      handler() {
+        this.loadCurrentOrderInfo();
+      },
+      immediate: true
+    }
+  },
   created() {
     if (this.$store.state.orderInfo && this.$store.state.orderInfo.id === this.$route.params.id) {
       return;
     }
-    this.$store.dispatch("loadOrderInfo", this.$route.params.id);
+    this.loadCurrentOrderInfo();
   },
   filters: { NumberFormat }
 };
 </script>
 
-<style lang="css" scoped></style>
+<style scoped>
+.info {
+  height: 400px;
+  margin-left: 400px;
+}
+</style>
